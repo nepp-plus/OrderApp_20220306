9c3d15af1afb30437246217caa8b8d6b4be3e399
nepp.plus <nepp.plus@gmail.com>
2022-03-06 14:49:22 +0900

    getCount / getItem 함수 구현

--- app/src/main/java/com/neppplus/orderapp_20220306/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/orderapp_20220306/adapters/MainViewPagerAdapter.kt
@@ -3,13 +3,18 @@ package com.neppplus.orderapp_20220306.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.neppplus.orderapp_20220306.fragments.MyProfileFragment
+import com.neppplus.orderapp_20220306.fragments.PizzaStoreListFragment
 
 class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
-    override fun getCount(): Int {
-
-    }
+    override fun getCount() = 2
 
     override fun getItem(position: Int): Fragment {
+
+        return when(position) {
+            0 -> PizzaStoreListFragment()
+            else -> MyProfileFragment()
+        }
 
     }
 }
