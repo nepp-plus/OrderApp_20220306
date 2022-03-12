66c54036cc4c4d6b04b61e2a1330acd4997ec95c
nepp.plus <nepp.plus@gmail.com>
2022-03-12 10:38:45 +0900

    첨부한 가게 데이터 수신 > UI에 데이터 반영  - Glide로 이미지 로드  - 레이팅바의 점수 표현

--- app/src/main/java/com/neppplus/orderapp_20220306/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/neppplus/orderapp_20220306/ViewStoreDetailActivity.kt
@@ -2,12 +2,18 @@ package com.neppplus.orderapp_20220306
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.neppplus.orderapp_20220306.datas.StoreData
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : AppCompatActivity() {
+
+    lateinit var mStoreData: StoreData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_store_detail)
+        mStoreData = intent.getSerializableExtra("store") as StoreData
         setupEvents()
         setValues()
     }
@@ -17,6 +23,13 @@ class ViewStoreDetailActivity : AppCompatActivity() {
     }
 
     fun setValues() {
+
+        txtName.text = mStoreData.name
+        Glide.with(this).load(mStoreData.logoUrl).into(imgLogo)
+        txtPhoneNum.text = mStoreData.phoneNum
+
+        scoreRatingBar.rating = mStoreData.score.toFloat()
+        txtScore.text = "(${mStoreData.score})"
 
     }
 
