2e70ea5dc98be598eab53d0b929c73987c8eab04
nepp.plus <nepp.plus@gmail.com>
2022-03-12 11:18:38 +0900

    닉네임을 입력 하고 돌아오면 > 프래그먼트에서 받아서 적용

--- app/src/main/java/com/neppplus/orderapp_20220306/EditNicknameActivity.kt
+++ app/src/main/java/com/neppplus/orderapp_20220306/EditNicknameActivity.kt
@@ -1,11 +1,34 @@
 package com.neppplus.orderapp_20220306
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+        setupEvents()
+        setValues()
     }
+
+    fun setupEvents() {
+
+        btnSave.setOnClickListener {
+
+            val inputNickname = edtNickname.text.toString()
+            val resultIntent = Intent()
+            resultIntent.putExtra("nickname", inputNickname)
+            setResult(RESULT_OK, resultIntent)
+            finish()
+
+        }
+
+    }
+
+    fun setValues() {
+
+    }
+
 }
--- app/src/main/java/com/neppplus/orderapp_20220306/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/orderapp_20220306/fragments/MyProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.neppplus.orderapp_20220306.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -29,6 +30,22 @@ class MyProfileFragment : Fragment() {
 
             val myIntent = Intent(requireContext(), EditNicknameActivity::class.java)
             startActivityForResult(myIntent, REQ_CODE_NICKNAME)
+
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQ_CODE_NICKNAME) {
+
+            if (resultCode == Activity.RESULT_OK) {
+
+                val resultNickname =  data!!.getStringExtra("nickname")
+                txtNickname.text = resultNickname
+
+            }
 
         }
 
