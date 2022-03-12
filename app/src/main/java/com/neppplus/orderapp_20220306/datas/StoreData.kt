c634f0887bfcbc7bb302762c35716d3e9c1fe89c
nepp.plus <nepp.plus@gmail.com>
2022-03-12 10:29:02 +0900

    리스트뷰 아이템 클릭 - 가게 데이터 첨부 + 화면 이동

--- app/src/main/java/com/neppplus/orderapp_20220306/datas/StoreData.kt
+++ app/src/main/java/com/neppplus/orderapp_20220306/datas/StoreData.kt
@@ -1,9 +1,11 @@
 package com.neppplus.orderapp_20220306.datas
 
+import java.io.Serializable
+
 class StoreData(
     val name: String,
     val score: Double,
     val phoneNum: String,
     val logoUrl: String,
-) {
+) : Serializable {
 }
